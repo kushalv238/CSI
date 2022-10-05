3ffae33729777e00175871457bcb67f43d0dce18
kushalv238 <kushalv238@gmail.com>
2022-10-05 11:32:22 +0530

    commit-3
    added a new 'Pixel Paranoia page'
    made responsive changes and added designs to the home page

--- script/responsive.js
+++ script/responsive.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('.burger')
 const navbar = document.querySelector('.navbar')
+const header_ = document.querySelector('.height')
 
 burger.addEventListener('click', ()=> {
     navbar.classList.toggle('o-nav')
+    header_.classList.toggle('height')
 })
--- script/script.js
+++ script/script.js
@@ -1,4 +1,32 @@
+const header = document.querySelector("header");
+const nav = document.querySelector('.navbar');
+
 window.addEventListener("scroll", function() {
-   const header = document.querySelector("header");
    header.classList.toggle("notTop", window.scrollY > 50);
+   // for(let i = 0; i < burgerLine.length; i++) {
+   //    burgerLine[i].classList.toggle("lineCol1 lineCol2", window.scrollY > 50);
+   //    burgerLine[i].classList.toggle("lineCol2", window.scrollY > 50);
+   // }
+
+   // $('[data-burger="line"]').toggleClass('lineCol1 lineCol2', ($(window).scrollTop() > 50));
+
+   if ($(window).scrollTop() > 50){
+      $('[data-burger="line"]').addClass( "lineCol2");
+      $('[data-burger="line"]').removeClass("lineCol1");
+      $('.navbar').addClass("scrolledNav");
+   }
+   else {
+      $('[data-burger="line"]').addClass( "lineCol1");
+      $('[data-burger="line"]').removeClass("lineCol2");
+      $('.navbar').removeClass("scrolledNav");
+   }
 }) 
+
+$(document).ready(function() {
+   $('.dropdown').click(function() {
+      $('.menu_').toggleClass('menu showMenu');
+   })
+   $('.dropdown_').hover(function() {
+      $('.menu_2').toggleClass('menu2 showMenu2');
+   })
+})
